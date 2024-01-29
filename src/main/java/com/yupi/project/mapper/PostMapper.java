34b273abe5d967b3515a107429f2d0ca2307d60f
Thetu <1652150116@qq.com>
2024-01-29 18:17:04 +0800

    项目初始化

---
+++ src/main/java/com/yupi/project/mapper/PostMapper.java
@@ -0,0 +1,12 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.project.model.entity.Post;
+
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/project/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.project.model.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+

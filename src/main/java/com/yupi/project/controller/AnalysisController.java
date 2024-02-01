60630ba2bf2fe8fa4f613529276ba388031e4203
Thetu <1652150116@qq.com>
2024-02-02 01:03:37 +0800

    接口统计分析功能开发

---
+++ src/main/java/com/yupi/project/controller/AnalysisController.java
@@ -0,0 +1,62 @@
+package com.yupi.project.controller;
+
+import cn.hutool.core.collection.CollectionUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.tu.apicommon.model.entity.InterfaceInfo;
+import com.tu.apicommon.model.entity.UserInterfaceInfo;
+import com.yupi.project.annotation.AuthCheck;
+import com.yupi.project.common.BaseResponse;
+import com.yupi.project.common.ErrorCode;
+import com.yupi.project.common.ResultUtils;
+import com.yupi.project.exception.BusinessException;
+import com.yupi.project.mapper.UserInterfaceInfoMapper;
+import com.yupi.project.model.vo.InterfaceInfoVO;
+import com.yupi.project.service.InterfaceInfoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/analysis")
+@Slf4j
+public class AnalysisController {
+
+    @Resource
+    private UserInterfaceInfoMapper userInterfaceInfoMapper;
+
+    @Resource
+    private InterfaceInfoService interfaceInfoService;
+
+    @GetMapping("/top/interface/invoke")
+    @AuthCheck(mustRole = "admin")
+    public BaseResponse<List<InterfaceInfoVO>> listTopInvokeInterfaceInfo() {
+        // 获取调用次数较多的接口
+        List<UserInterfaceInfo> userInterfaceInfoList = userInterfaceInfoMapper.listTopInvokeInterfaceInfo(3);
+        // 根据接口id分组
+        Map<Long, List<UserInterfaceInfo>> interfaceInfoIds = userInterfaceInfoList.stream()
+                .collect(Collectors.groupingBy(UserInterfaceInfo::getInterfaceInfoId));
+        QueryWrapper<InterfaceInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.in("id", interfaceInfoIds.keySet());
+        List<InterfaceInfo> interfaceInfoList = interfaceInfoService.list(queryWrapper);
+        if (CollectionUtil.isEmpty(interfaceInfoList)) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+        }
+        List<InterfaceInfoVO> interfaceInfoVOList = interfaceInfoList.stream().map(interfaceInfo -> {
+            InterfaceInfoVO interfaceInfoVO = new InterfaceInfoVO();
+            BeanUtils.copyProperties(interfaceInfo, interfaceInfoVO);
+            Integer totalNum = interfaceInfoIds.get(interfaceInfo.getId()).get(0).getTotalNum();
+            interfaceInfoVO.setTotalNum(totalNum);
+            return interfaceInfoVO;
+        }).collect(Collectors.toList());
+        return ResultUtils.success(interfaceInfoVOList);
+    }
+
+
+}
--- src/main/java/com/yupi/project/mapper/UserInterfaceInfoMapper.java
+++ src/main/java/com/yupi/project/mapper/UserInterfaceInfoMapper.java
@@ -4,6 +4,8 @@ package com.yupi.project.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tu.apicommon.model.entity.UserInterfaceInfo;
 
+import java.util.List;
+
 /**
 * @author The tu
 * @description 针对表【user_interface_info(用户调用接口关系)】的数据库操作Mapper
@@ -12,6 +14,12 @@ import com.tu.apicommon.model.entity.UserInterfaceInfo;
 */
 public interface UserInterfaceInfoMapper extends BaseMapper<UserInterfaceInfo> {
 
+    /**
+     * 获取调用次数排名靠前的接口
+     * @param limit
+     * @return
+     */
+    List<UserInterfaceInfo> listTopInvokeInterfaceInfo(int limit);
 }
 
 
